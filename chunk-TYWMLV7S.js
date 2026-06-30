7e9eea506769a88437e558ca1cc1e5e4e19260ee
Hekroma <30846247+Hekroma@users.noreply.github.com>
2026-06-30 11:36:51 +0300

    Auto-generated commit

---
+++ chunk-TYWMLV7S.js
@@ -0,0 +1,1 @@
+import{Na as o,Oa as i,Ra as n,xa as a}from"./chunk-V6XJSZMP.js";var r=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=a({type:t,selectors:[["app-favourite"]],decls:2,vars:0,template:function(e,p){e&1&&(o(0,"p"),n(1,"favourite works!"),i())},encapsulation:2})};export{r as Favourite};
